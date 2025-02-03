6623f2411fa1d48a401fa3b716cfc1e39903d1aa
Omkar Tipugade <omtipugade3904@gmail.com>
2025-02-03 21:16:30 +0530

    Fix event resizing functionality in schedule table

--- src/components/ScheduleTable.jsx
+++ src/components/ScheduleTable.jsx
@@ -250,7 +250,7 @@ const ScheduleTable = ({ resources, currentDate }) => {
                           id="resizable"
                           className={`resizable m-1 p-1 text-xs rounded ${
                             event.color
-                          } cursor-pointer flex justify-between absolute group ${getTextBgColor(
+                          } cursor-pointer flex justify-between relative group ${getTextBgColor(
                             event.color
                           )}`}
                           onMouseEnter={() =>
@@ -276,7 +276,7 @@ const ScheduleTable = ({ resources, currentDate }) => {
                           <div className="content flex-grow px-2 relative z-10">
                             {event.name}
                           </div>
-                          <div onMouseDown={(event,rightHandle)} className="handle right-handle cursor-ew-resize h-full absolute w-1 right-0 top-0 opacity-[.3] bg-black select-none transition-opacity-[0.2s] hover:opacity-100"></div>
+                          <div onMouseDown={(event)=>{startResize(event,rightHandle)}} className="handle right-handle cursor-ew-resize h-full absolute w-1 right-0 top-0 opacity-[.3] bg-black select-none transition-opacity-[0.2s] hover:opacity-100"></div>
                         </div>
                       ))}
                   </td>
